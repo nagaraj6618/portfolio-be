0df168bf8b564580049eac2eac1c5828a1c44c0e
NAGARAJ S <nagaraj516700@gmail.com>
2024-03-22 23:38:37 +0530

    Feat : Added delete Option and Auto Email Generation

--- controller/contactController.js
+++ controller/contactController.js
@@ -1,5 +1,12 @@
 const ContactSchema = require('../model/contact.js');
-
+const nodemailer = require('nodemailer');
+let transporter = nodemailer.createTransport({
+   service: 'Gmail',
+   auth: {
+       user: 'nagaraj516700@gmail.com', 
+       pass: 'bgxm fbbf gofe rlbp' 
+   }
+});
 const getOneContactInfo = async(req,res) => {
    try{
    const contactData = await ContactSchema.findById(req.params.id)
@@ -29,7 +36,25 @@ const postContactInfo = async(req,res) => {
    try{
       const newPost = new ContactSchema(req.body)
       await newPost.save()
-      // console.log(req.body);
+      let mailOptions = {
+         from: 'nagaraj516700@gmail.com', 
+         to: req.body.email, 
+         subject: "Thanks for contacting! We'll get back to you soon.", 
+         text: `Hii,
+      
+      I am Nagaraj S. Thank you for reaching out! Your message has been received, and I will get back to you as soon as possible.
+      
+      Best regards,
+      Nagaraj S,
+      secretary` 
+      };
+      transporter.sendMail(mailOptions, function(error, info){
+         if (error) {
+             console.error('Error sending email:', error);
+         } else {
+             console.log('Email sent:', info.response);
+         }
+     });
       res.status(201).json({message:'Success',data:req.body});
    }
    catch(error){
@@ -37,4 +62,16 @@ const postContactInfo = async(req,res) => {
    }
    
 }
-module.exports = {getAllContactInfo , postContactInfo ,getOneContactInfo};
+
+const deleteOneInfo = async(req,res) => {
+   const id = req.params.id;
+   try{
+      
+      const deletedContact = await ContactSchema.deleteOne({_id:id})
+      res.status(200).json({message:"Contact deleted successfully", data: deletedContact});
+   }
+   catch(error){
+      res.status(500).json({message:'unsuccess'});
+   }
+}
+module.exports = {getAllContactInfo , postContactInfo ,getOneContactInfo,deleteOneInfo};
--- route/contactRoute.js
+++ route/contactRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {getAllContactInfo,postContactInfo , getOneContactInfo} = require('../controller/contactController.js')
+const {getAllContactInfo,postContactInfo , getOneContactInfo,deleteOneInfo} = require('../controller/contactController.js')
 const router = express.Router();
 
 router.get('/',getAllContactInfo);
 router.post('/',postContactInfo);
 
 router.get('/:id',getOneContactInfo)
+router.delete('/:id',deleteOneInfo)
 module.exports = router;
