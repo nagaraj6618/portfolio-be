d8b088d0f63df5b3dfec467a4b4ba13a79075c72
NAGARAJ S <nagaraj516700@gmail.com>
2024-03-25 22:29:13 +0530

    Feat : Added verification for admin registeration

--- route/authRoute.js
+++ route/authRoute.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {checkingMethod,registerMethod,loginMethod} = require("../controller/authController.js")
-
+const { checkingMethod,registerMethod,loginMethod } = require("../controller/authController.js")
+const { verifyAdmin } = require('../controller/authController.js')
 router.get('/',checkingMethod);
-router.post('/register',registerMethod);
+router.post('/register',verifyAdmin,registerMethod);
 router.post('/login',loginMethod);
 module.exports = router;
 
